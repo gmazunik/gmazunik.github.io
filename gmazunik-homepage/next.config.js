8fd22c47052f0e1f48dff1ed3aa167113cdf074a
gmazunik <5315162+gmazunik@users.noreply.github.com>
2024-01-08 09:46:13 -0600

    Begin NextJS app implementation

---
+++ gmazunik-homepage/next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+
+const DEPLOYED_TO_GITHUB_PAGES = process.env.NODE_ENV === 'production';
+
+const repoName = 'gmazunik.github.io';
+
+const basePath = DEPLOYED_TO_GITHUB_PAGES ? `/${repoName}` : '';
+const assetPrefix = DEPLOYED_TO_GITHUB_PAGES ? `/${repoName}/` : '';
+
+const nextConfig = {
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
+    reactStrictMode: true,
+    basePath,
+    assetPrefix,
+}
+
+module.exports = nextConfig
